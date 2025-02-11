9dcf2587653b9e1e13d75287d55eef331eb69692
sandeep-garai <79070318+sandeep-garai@users.noreply.github.com>
2025-02-11 19:15:41 +0530

    Flask Tutorial

---
+++ flask_blog.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app= Flask(__name__)
+
+post= [
+    {
+        'author':'Sandeep Garai',
+        'title':'First Tutorial',
+        'content': 'blog post 1'
+    },
+    {
+        'author':'S Garai',
+        'title':'Second Tutorial',
+        'content': 'blog post 2'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('home.html', posts=post)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
